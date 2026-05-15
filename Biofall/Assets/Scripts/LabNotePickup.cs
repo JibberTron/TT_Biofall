9cf90f8bc39b42a40f56c7ad57ebf8eaf1496b1e
goldmoufgoddess <goldmoufgoddess@outlook.com>
2026-05-14 21:10:26 -0500

    Add lab note pickup interaction

--- Biofall/Assets/Scripts/LabNotePickup.cs
+++ Biofall/Assets/Scripts/LabNotePickup.cs
@@ -9,15 +9,52 @@ public class LabNotePickup : MonoBehaviour, IInteractable
 
     [SerializeField] bool destroyAfterPickup = true;
 
+    [SerializeField] GameObject notePanel;
+
+    private bool playerNearby;
+  void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+        
+        if(notePanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            notePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
     public void Interact()
     {
         Debug.Log("Lab Note Found: " + noteTitle);
 
         Debug.Log(noteText);
 
-        if(destroyAfterPickup)
+        notePanel.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        if (destroyAfterPickup)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            Debug.Log("Press E to read lab note");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
         }
     }
 }

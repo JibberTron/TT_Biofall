b7735343528ef54cec94b146bed7c13e3be762a1
goldmoufgoddess <goldmoufgoddess@outlook.com>
2026-05-24 09:08:13 -0500

    PlayerInventory/PickupItem scripts updated

--- Biofall/Assets/Scripts/PickupItem.cs
+++ Biofall/Assets/Scripts/PickupItem.cs
@@ -13,13 +13,26 @@ public class PickupItem : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+
         switch (itemType)
         {
             case ItemType.Flashlight:
+                if (inventory != null)
+                {
+                    inventory.GiveFlashlight();
+                }
+
                 Debug.Log("Flashlight picked up!");
                 break;
 
             case ItemType.Key:
+                if (inventory != null)
+                {
+                    inventory.AddKey(1);
+                }
+
                 Debug.Log("Key picked up!");
                 break;
         }
